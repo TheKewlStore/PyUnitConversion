7acbef865fb47d022f3bcfceacd813b074e0a8c6
Ian Davis <Ian.Davis@hobartcorp.com>
2016-04-26 16:20:24 -0400

    Restructured project and added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='unit_conversion',
+    version='1.0.0',
+    packages=('unit_conversion', ),
+    install_requires=['unit_conversion', ],
+    url = 'https://github.com/TheKewlStore/PySFTPClient',
+    license = '',
+    author = 'Ian Davis',
+    author_email = 'thekewlstore@gmail.com',
+    description = "An API to make SFTP file transfers through chilkat simpler and more pythonic",
+    )
